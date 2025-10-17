8f609d2b2899fd0fc6b9ef6c63569cbccdb31d53
Russell Montjoy <russellmontjoy@gmail.com>
2025-10-17 16:14:06 -0600

    inital start

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import {useState} from "react"; 
+import {tvshows} from "./shows/data"; 
+import ShowDetails from "./shows/ShowDetails"; 
+import ShowSelection from "./shows/ShowSelection";
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+
+  const [shows] = useState(tvshows); 
+  const [selectedShow, setSelectedShow] = useState(); 
+
+
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection 
+        shows={shows}
+        selectedShow = {selectedShow}
+        setSelectedShow = {setSelectedShow}
+        />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails key={selectedShow?.name} show={selectedShow}/>
+      </main>
     </>
   );
 }
